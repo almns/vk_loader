3ca7a70222cd5c236697939832e73568c8d04a54
Alex <example@example.com>
2014-11-10 13:42:50 +0300

    Grab only social graph, not full profiles

--- src/vkapi.py
+++ src/vkapi.py
@@ -8,14 +8,15 @@ import json
 
 logger = logging.getLogger(__name__)
 
-class VkError(Exception):
+class VkError(Exception):    
     pass
 
-class VkAPI(object):    
-  
-    PROFILE_FIELDS = ','.join(['nickname', 'screen_name', 'sex', 'bdate', 'city', 'relation', 'country', 'education', 
-                               'universities', 'schools', 'connections', 'relation', 'relatives', 'interests', 'books'])
+
+PROFILE_FIELDS = ','.join(['nickname', 'screen_name', 'sex', 'bdate', 'city', 'relation', 'country', 'education', 'counters', 'home_town',
+                               'universities', 'schools', 'connections', 'relation', 'relatives', 'interests', 'books', 'last_seen'])    
     
+class VkAPI(object):    
+         
     def __init__(self, token=None):
         self.session = requests.Session()
         self.session.headers['Accept'] = 'application/json'
@@ -58,20 +59,20 @@ class VkAPI(object):
             self.requests_times = self.requests_times[1:]            
         self.requests_times.append(time.time())
         
-    def get_user_profile(self, user_id):    
-        profile = self._do_api_call('users.get', { 'user_ids' :  user_id,  'fields' : VkAPI.PROFILE_FIELDS})                    
+    def get_user_profile(self, user_id, fields=PROFILE_FIELDS):    
+        profile = self._do_api_call('users.get', { 'user_ids' :  user_id,  'fields' : fields})                    
         return profile[0]           
                             
-    def get_user_profiles(self, user_ids):                      
+    def get_user_profiles(self, user_ids, fields=PROFILE_FIELDS):                      
         result = []        
         for offset in xrange(0, len(user_ids) / 100 + 1):            
             start, end = offset * 100, (offset + 1) * 100 
             ids = ','.join([str(user_id) for user_id in user_ids[start:end]])        
-            response = self._do_api_call('users.get', { 'user_ids' :  ids,  'fields' : VkAPI.PROFILE_FIELDS})
+            response = self._do_api_call('users.get', { 'user_ids' :  ids,  'fields' : fields})
             result.extend(response)
         return result
     
-    def get_group_users(self, group_id):
+    def get_group_users(self, group_id, fields=PROFILE_FIELDS):
         members_count = self._do_api_call('groups.getById', { 'group_id' :  group_id,  'fields' : 'members_count'})[0]['members_count'] 
         user_ids = set()
         for offset in xrange(0, members_count / 1000 + 1):
@@ -79,8 +80,8 @@ class VkAPI(object):
             user_ids.update(response['items'])
         return list(user_ids)
     
-    def get_friends(self, user_id):
-        response = self._do_api_call('friends.get', { 'user_id' : user_id,   'fields' : VkAPI.PROFILE_FIELDS})                    
+    def get_friends(self, user_id, fields=PROFILE_FIELDS):
+        response = self._do_api_call('friends.get', { 'user_id' : user_id,   'fields' : fields})                    
         return response['items']
                     
     def close():
@@ -88,29 +89,32 @@ class VkAPI(object):
 
 def get_user_network(user_id, depth):    
     api = VkAPI()       
-    all_profiles = dict()       
-        
-    def load_user_network(user_id, depth):    
-        if depth == 0:
-            return
-            
-        if user_id not in all_profiles:
-            logger.info('Getting profile for id%s' % user_id)
-            all_profiles[user_id] = api.get_user_profile(user_id)                     
-                
-        logger.info('Getting friends for id%s' % user_id)    
-        friends = api.get_friends(user_id)
-        for friend in friends:
-            friend_id = int(friend['id'])
-            if friend_id not in all_profiles:
-                all_profiles[friend_id] = friend
-                load_user_network(friend_id, depth - 1)                                
-        all_profiles[user_id]['friends'] = [int(friend['id']) for friend in friends]        
+    all_profiles = dict()
+    logger.info('Getting profile for id%s' % user_id)
+    all_profiles[user_id] = api.get_user_profile(user_id)
     
-    load_user_network(user_id, depth)    
-    return all_profiles 
+    queue = [(user_id, depth)]       
+    while len(queue) > 0:
+        head_id, head_depth = queue[0]        
+        logger.info('Getting friends for id%s' % head_id)   
+        friends = []
+        try:
+            friends = api.get_friends(head_id, fields=[])    
+        except VkError as e:
+            pass
         
-
+        all_profiles[head_id]['friends'] = [int(friend['id']) for friend in friends] 
+        
+        if head_depth > 1:            
+            for friend in friends:
+                friend_id = int(friend['id'])
+                if friend_id not in all_profiles:
+                    all_profiles[friend_id] = friend
+                    queue.append((friend_id, head_depth - 1))
+        queue.pop(0) 
+    return all_profiles 
+    
+    
 if __name__ == '__main__':
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
@@ -120,7 +124,8 @@ if __name__ == '__main__':
     except:
         pass
         
-    for user_id in sys.argv[1:]:     
+    for user_id in open(sys.argv[1], 'r') if os.path.isfile(sys.argv[1]) else sys.argv[1:]:   
+        user_id = user_id.strip()
         logger.info('Getting network for id%s' % user_id)
         with codecs.open('data/egonet_%s.json' % user_id, 'w', 'utf-8') as f:
             user_network = get_user_network(user_id, 2)
