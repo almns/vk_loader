9da3334f7bfd0770a6b18e1de370d7cb638ade59
Alex <example@example.com>
2014-12-14 13:11:36 +0300

    Download only egonet for the user

--- src/vkapi.py
+++ src/vkapi.py
@@ -13,7 +13,7 @@ class VkError(Exception):
 
 
 PROFILE_FIELDS = ','.join(['nickname', 'screen_name', 'sex', 'bdate', 'city', 'relation', 'country', 'education', 'counters', 'home_town',
-                               'universities', 'schools', 'connections', 'relation', 'relatives', 'interests', 'books', 'last_seen'])    
+                               'universities', 'schools', 'connections', 'relation', 'relatives', 'interests', 'books', 'last_seen', 'occupation'])    
     
 class VkAPI(object):    
          
@@ -87,46 +87,68 @@ class VkAPI(object):
     def close():
         self.session.close()        
 
-def get_user_network(user_id, depth):    
-    api = VkAPI()       
-    all_profiles = dict()
-    logger.info('Getting profile for id%s' % user_id)
-    all_profiles[user_id] = api.get_user_profile(user_id)
-    
-    queue = [(user_id, depth)]       
-    while len(queue) > 0:
-        head_id, head_depth = queue[0]        
-        logger.info('Getting friends for id%s' % head_id)   
-        friends = []
-        try:
-            friends = api.get_friends(head_id, fields=[])    
-        except VkError as e:
-            pass
+    def get_user_network(self, user_id, depth):          
+        all_profiles = dict()
+        logger.info('Getting profile for id%s' % user_id)
+        all_profiles[user_id] = self.get_user_profile(user_id)
         
-        all_profiles[head_id]['friends'] = [int(friend['id']) for friend in friends] 
-        
-        if head_depth > 1:            
-            for friend in friends:
-                friend_id = int(friend['id'])
-                if friend_id not in all_profiles:
-                    all_profiles[friend_id] = friend
-                    queue.append((friend_id, head_depth - 1))
-        queue.pop(0) 
-    return all_profiles 
-    
+        queue = [(user_id, depth)]       
+        while len(queue) > 0:
+            head_id, head_depth = queue[0]        
+            logger.info('Getting friends for id%s' % head_id)   
+            friends = []
+            try:
+                friends = self.get_friends(head_id, fields=[])    
+            except VkError as e:
+                pass
+            
+            all_profiles[head_id]['friends'] = [int(friend['id']) for friend in friends] 
+            
+            if head_depth > 1:            
+                for friend in friends:
+                    friend_id = int(friend['id'])
+                    if friend_id not in all_profiles:
+                        all_profiles[friend_id] = friend
+                        queue.append((friend_id, head_depth - 1))
+            queue.pop(0) 
+        return all_profiles 
+       
+def save_friends_pairs_file(file, user_network):   
+    with open(file, 'w') as of:
+        pairs = set()
+        for k, v in user_network.iteritems():
+            k = int(k)
+            for fr in v['friends']:
+                if fr in user_network:                    
+                    if (k, fr) not in pairs:
+                        of.write('%d %d\n' % (k, fr))
+                        pairs.add((k, fr))
+                    if (fr, k) not in pairs:
+                        of.write('%d %d\n' % (fr, k))
+                        pairs.add((fr, k))   
+                    
+def save_profiles_file(file, user_network):   
+    with codecs.open(file, 'w', 'utf-8') as of:
+        of.write(json.dumps(user_network, ensure_ascii=False, encoding='utf-8', indent=1)) 
+                
     
 if __name__ == '__main__':
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
+       
+    api = VkAPI()    
     
-    try:        
-        os.mkdir('data')
-    except:
-        pass
-        
     for user_id in open(sys.argv[1], 'r') if os.path.isfile(sys.argv[1]) else sys.argv[1:]:   
         user_id = user_id.strip()
         logger.info('Getting network for id%s' % user_id)
-        with codecs.open('data/egonet_%s.json' % user_id, 'w', 'utf-8') as f:
-            user_network = get_user_network(user_id, 2)
-            f.write(json.dumps(user_network, ensure_ascii=False, encoding='utf-8', indent=1))        
+        
+        user_network = api.get_user_network(user_id, 2)   
+        
+        interested_user_ids = set()
+        for uid, profile in user_network.iteritems():                             
+           interested_user_ids.add(uid) # profile['friends']
+        user_profiles = api.get_user_profiles(list(interested_user_ids))
+                
+        save_profiles_file('profiles_%s.json' % user_id, user_profiles)        
+        save_friends_pairs_file('egonet_%s.csv' % user_id, user_network)
+        
